c77a7ef3513860121d27497655339276733a40de
MagnusDaroe <magnusdaroe@gmail.com>
2023-04-25 15:15:37 +0200

    Work with terminal

--- Arduino/DispenserTests/Python-ArduinoComunicator.py
+++ Arduino/DispenserTests/Python-ArduinoComunicator.py
@@ -5,6 +5,7 @@ import time
 ser = serial.Serial()
 
 # Search for available serial ports
+
 def find_serial_port():
     import serial.tools.list_ports
     ports = list(serial.tools.list_ports.comports())
@@ -20,6 +21,11 @@ while True:
         ser.baudrate = 9600
         ser.open()
         break
+    else:
+        print("No Arduino board found. Please make sure it is connected.")
+        time.sleep(5)
+
+
 
 
 while True:
