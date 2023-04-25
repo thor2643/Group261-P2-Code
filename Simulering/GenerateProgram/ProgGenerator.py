39b9f3b97c8e9e27cd1a29579d1119b4948db734
thor2643 <Thor951753@hotmail.com>
2023-04-25 12:49:08 +0200

    More changes

--- Simulering/GenerateProgram/ProgGenerator.py
+++ Simulering/GenerateProgram/ProgGenerator.py
@@ -20,8 +20,8 @@ joints1 = np.array([0, -90, -90, 0, 90, 0])
 joints2 = np.array([78, -48, -150, -138, 265, 0])
 
 #Create parabolic blend function
-f1, t0, tf = TPPlanner.get2PointMoveLFunction(joints1, joints2, 400, 0, 0)
-
+f1, t0, tf = TPPlanner.get2PointMoveLFunction(joints1, joints2, 100, 0, 0)
+print(tf)
 target = None
 
 new_program = 1
@@ -33,24 +33,34 @@ if new_program:
     # Very important: Make sure we set the reference frame and tool frame so that the robot is aware of it
     program.setPoseFrame(ref_frame)
     program.setPoseTool(robot.PoseTool())
-    program.setSpeed(100, 50, 1 1)   #linear vel and joint vel
+    program.setSpeed(1, 500)   #linear vel and joint vel
     idx = 0
+    idx1 = 50
+
 
     #Divide the trajectory into 50 points
     for i in np.linspace(t0, tf, 100):
 
         joint_pose = list(f1(i))
 
-        #target = RDK.AddTarget('T%i' % idx, ref_frame)
-        #arget = target.setJoints(joint_pose)
+        target = RDK.AddTarget('T%i' % idx, ref_frame)
+        target = target.setJoints(joint_pose)
+        target.setAsJointTarget()
 
         #print(target.Pose())
         # Add a linear movement (with the exception of the first point which will be a joint movement)
         if i == 0:
             program.MoveJ(joint_pose)
         else:
-            program.MoveJ(joint_pose)
+            program.MoveJ(target)
 
         idx += 1
+        
+        if idx<50:
+            program.setSpeed(5000, idx/10)
+        else:
+            program.setSpeed(5000, idx1/10)
+            idx1 -=1
+    
 
 program.RunProgram()
